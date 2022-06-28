db4200e2e33745d6d6f45e3f72e176ce1cf24484
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-28 15:59:38 +0900

    feat:task削除機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,7 +34,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "タスクを削除しました"
   end
 
   private
