4e7424bd1c91d7ca5c90ae4f67d4ff03b61fc988
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-28 14:13:17 +0900

    feat:task機能new create作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end
