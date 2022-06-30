c22eb9c7288af4ec69456b067d492863e717ad57
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-30 15:20:09 +0900

    test:新しいタスクが一番上に表示テスト完了

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task)}
+  let!(:second_task) { FactoryBot.create(:second_task)}
+  let!(:third_task) { FactoryBot.create(:third_task)}
+  before do
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
         fill_in 'task[title]', with: 'test_title'
         fill_in 'task[content]', with: 'test_content'
-        click_button 'Create Task'
+        click_button '登録する'
         expect(page).to have_content 'test_title'
         expect(page).to have_content 'test_content'
       end
@@ -15,21 +20,25 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task)
         visit tasks_path
         expect(page).to have_content 'test_title'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        visit tasks_path
+        all('.task-show').first.click_link('詳細')
+        expect(page).to have_content 'test_title3'
       end
     end
   end
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task)
-        task = FactoryBot.create(:second_task)
-        task = FactoryBot.create(:third_task)
         visit tasks_path
         click_link '詳細', href: task_path(task)
         expect(page).to have_content 'test_title'
+        expect(page).to have_content 'test_content'
       end
     end
   end
