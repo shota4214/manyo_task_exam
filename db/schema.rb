9e0fc69d8368e6c4da729245ed2c13ce7e80b2b0
shota4214 <seshiruff4214@yahoo.co.jp>
2022-07-01 13:59:57 +0900

    feat:終了期限column追加・終了期限入力欄追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_28_051423) do
+ActiveRecord::Schema.define(version: 2022_07_01_023235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_06_28_051423) do
     t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "deadline", default: -> { "now()" }, null: false
   end
 
 end
