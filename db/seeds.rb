43706418721903a5c679a8acf727545f88e9f1a6
shota4214 <seshiruff4214@yahoo.co.jp>
2022-07-11 13:43:20 +0900

    herokuデプロイエラー対応

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(name:"shota", email:"seshiruff4214@yahoo.co.jp", password_digest:"12345678", admin:"true")
+User.create!(name:"shota", email:"seshiruff4214@yahoo.co.jp", password: "123456", admin:"true")
 10.times do |n|
-  User.create!(name:"ユーザー#{n + 1}", email:"test#{n + 1}@test.com", password_digest:"12345678")
+  User.create!(name:"ユーザー#{n + 1}", email:"test#{n + 1}@test.com", password: "123456")
 end
